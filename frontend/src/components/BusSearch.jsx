fd170473534ef6f17c536ca6da9303d0941b3962
srabon007-9 <srabonahmed2002@gmail.com>
2026-05-07 02:19:30 +0600

    removed unused files

--- frontend/src/components/BusSearch.jsx
+++
@@ -1,7 +0,0 @@
-import SearchBar from './SearchBar';
-
-function BusSearch({ onSearch }) {
-  return <SearchBar placeholder="Search by bus name, route, or location..." onSearch={onSearch} />;
-}
-
-export default BusSearch;
--- frontend/src/components/FloatingPanel.jsx
+++
@@ -1,7 +0,0 @@
-export default function FloatingPanel({ children }) {
-  return (
-    <div className="absolute left-4 top-4 z-40 w-full max-w-sm rounded-2xl border border-white/20 bg-slate-950/95 p-4 shadow-2xl">
-      {children}
-    </div>
-  );
-}
